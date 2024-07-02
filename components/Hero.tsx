2a17f2fd029f84f70e9989f173d6eb4572fd053a
chabieralaiza <calaiza64@gmail.com>
2024-07-02 20:17:03 +0200

    projects

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
             </div>
             <div className="flex justify-center relative my-20 z-10">
                 <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-                    <img src="/logo-blanco.png" width={200} />
+                    <img src="/logo-blanco.png" width={220} />
                     <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
                         Bienvenido a mi portfolio
                     </h2>
